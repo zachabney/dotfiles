c64abedec1755ef3c361c609794d26b72dc74d25
Zach Abney <zachabney@gmail.com>
2026-07-19 20:35:58 -0500

    Update nvim

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -1,1 +1,12 @@
 vim.treesitter.start()
+
+vim.lsp.config("rust_analyzer", {
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = {
+                target = "riscv32imac-unknown-none-elf",
+                allTargets = false,
+            },
+        },
+    },
+})
